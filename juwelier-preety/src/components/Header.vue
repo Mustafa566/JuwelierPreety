731b8a9fa2b1e2a12108692a78c31e659320bfb9
Mustafa566 <50444816+Mustafa566@users.noreply.github.com>
2021-04-16 00:22:33 +0200

    Bootstrap, vue-head installed

---
+++ juwelier-preety/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-row class="bgHeader">
+    <b-col>1 of 3</b-col>
+    <b-col>2 of 3</b-col>
+    <b-col>3 of 3</b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bgHeader {
+  background-color: #E4E4E4;
+}
+</style>
--- juwelier-preety/src/main.js
+++ juwelier-preety/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import VueHead from 'vue-head'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
+Vue.use(VueHead)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
